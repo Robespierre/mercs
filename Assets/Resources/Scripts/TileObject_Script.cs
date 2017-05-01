53b49413c7ada328bf39fc908b444b97c09c58f5
Robespierre <symalnev@gmail.com>
2017-05-01 14:45:56 +0300

    +fix moving when pressing the tile

--- Assets/Resources/Scripts/TileObject_Script.cs
+++ Assets/Resources/Scripts/TileObject_Script.cs
@@ -13,22 +13,18 @@ public class TileObject_Script : MonoBehaviour
     private void Awake()
     {
         gameScript = GameObject.Find("GameManager").GetComponent<Game_Script>();
-        curField = GameObject.Find("GameManager").GetComponent<Field_Script>().currentField;
     }
 
     private void OnMouseUpAsButton()
     {
         if (gameScript.currentGameState == Game_Script.GameState.playerTurn && !gameScript.inputLocked)
         {
-            TileOccupant_Script occScript = gameScript.activeUnit.GetComponent<TileOccupant_Script>(); 
+            TileOccupant_Script occScript = gameScript.activeUnit.GetComponent<TileOccupant_Script>();
+            curField = GameObject.Find("GameManager").GetComponent<Field_Script>().currentField;
             //ADD CHECK IF IT'S MOVEMENT STATE FOR THE MERC
             if (curField == null)
             {
                 Debug.Log("cf");
-            }
-            if (curField == null)
-            {
-                Debug.Log("os");
             }
             StartCoroutine(curField.WalkThePath(curField.IntsToTile(occScript.tileI, occScript.tileJ), curField.IntsToTile(tileI, tileJ)));//DEBUG NEEDED
         }
